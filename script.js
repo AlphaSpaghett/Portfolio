bdf5e8f4dcffb829a7d1cc2cf70d2a005b25644f
Ross <rossumj@hotmail.com>
2021-05-09 17:03:54 -0400

    Section 4 done

--- script.js
+++ script.js
@@ -97,3 +97,93 @@ Array.from(aboutMeTextContent).forEach(char => {
     });
 });
 // End of About me Text
+
+// Projects
+const container = document.querySelector(".container");
+const projects = document.querySelectorAll(".project");
+const projectHideBtn = document.querySelector(".project-hide-btn");
+
+projects.forEach((project, i) => {
+    project.addEventListener("mouseenter", () => {
+        project.firstElementChild.style.top = 
+            `-${project.firstElementChild.offsetHeight - project.offsetHeight + 20}px`;
+    });
+
+    project.addEventListener("mouseleave", () => {
+        project.firstElementChild.style.top = "2rem";
+    });
+
+    // Big Project Image
+    project.addEventListener("click", () => {
+        const bigImgWrapper = document.createElement("div");
+        bigImgWrapper.className = "project-img-wrapper";
+        container.appendChild(bigImgWrapper);
+
+        const bigImg = document.createElement("img");
+        bigImg.className = "project-img";
+        const imgPath = project.firstElementChild.getAttribute("src").split(".")[0];
+        bigImg.setAttribute("src",`${imgPath}-big.jpg`);
+        bigImgWrapper.appendChild(bigImg);
+        document.body.style.overflowY = "hidden";
+
+        projectHideBtn.classList.add("change");
+
+        projectHideBtn.onclick = () => {
+            projectHideBtn.classList.remove("change");
+            bigImgWrapper.remove();
+            document.body.style.overflowY = "scroll";
+        };
+    });
+    // End of Big Project Image
+
+    i >= 6 && (project.style.cssText = "display: none; opacity: 0");
+});
+
+// Projects button
+const section3 = document.querySelector(".section-3");
+const projectBtn = document.querySelector(".projects-btn");
+const projectBtnText = document.querySelector(".projects-btn span");
+let showHideBool = true;
+
+const showProjects = (project, i) => {
+    setTimeout(() => {
+        project.style.display = "flex";
+        section3.scrollIntoView({block: "end"});
+    }, 600);
+
+    setTimeout(() => {
+        project.style.opacity = "1";                   
+    }, i * 200);
+}
+
+const hideProjects = (project, i) => {
+    setTimeout(() => {
+        project.style.display = "none";
+        section3.scrollIntoView({block: "end"});
+    }, 1200);
+
+    setTimeout(() => {
+        project.style.opacity = "0";                
+    }, i * 100);
+}
+
+projectBtn.addEventListener("click", (e) => {
+   e.preventDefault();
+
+   projectBtn.firstElementChild.nextElementSibling.classList.toggle("change");
+
+   showHideBool 
+    ? (projectBtnText.textContent = "Show Less") 
+    : (projectBtnText.textContent = "Show More");
+
+   projects.forEach((project, i) => {
+       i >= 6 && 
+        (showHideBool 
+            ? showProjects(project, i) 
+            : hideProjects(project, i)
+        );
+    });
+   showHideBool = !showHideBool;
+});
+// End of Projects button
+// End of Projects
