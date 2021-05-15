c7975f4d11705215b6d6140b32701cd4c85493f1
Ross <rossumj@hotmail.com>
2021-05-15 16:29:30 -0400

    Section 12 Finished

--- script.js
+++ script.js
@@ -2,9 +2,12 @@
 const mouseCircle = document.querySelector('.mouse-circle');
 const mouseDot = document.querySelector('.mouse-dot');
 
+let mouseCircleBool = true;
+
 const mouseCircleFn = (x,y) => {
-   mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
-   mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
+    mouseCircleBool && (mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`);    
+
+    mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
 };
 // End of Mouse Circle
 
@@ -47,12 +50,77 @@ const animateCircles = (e, x, y) => {
 }
 // End of Animated Circles
 
+let hoveredElPosition = [];
+
+const stickyEl  = (x, y, hoveredEl) => {
+    // Sticky Element
+    
+    if (hoveredEl.classList.contains("sticky")) {
+        hoveredElPosition.length < 1 && (hoveredElPosition =[hoveredEl.offsetTop, hoveredEl.offsetLeft]);
+        
+        hoveredEl.style.cssText = `top: ${y}px; left:${x}px`;
+
+        if (hoveredEl.offsetTop <= hoveredElPosition[0] - 100 ||
+            hoveredEl.offsetTop >= hoveredElPosition[0] + 100 ||
+            hoveredEl.offsetLeft <= hoveredElPosition[1] - 100 ||
+            hoveredEl.offsetLeft >= hoveredElPosition[1] + 100
+            ) {
+                hoveredEl.style.cssText = "";
+                hoveredElPosition = [];
+            }
+
+        hoveredEl.onmouseleave = () => {
+            hoveredEl.style.cssText = "";
+            hoveredElPosition = [];
+        }
+    }
+    // End of Sticky Element
+};
+
+// Mouse Circle Transform
+const mouseCircleTransform = (hoveredEl) => {
+    if (hoveredEl.classList.contains("pointer-enter")) {
+        hoveredEl.onmousemove = () => {
+            mouseCircleBool = false;
+            mouseCircle.style.cssText = `
+            width: ${hoveredEl.getBoundingClientRect().width}px;
+            height: ${hoveredEl.getBoundingClientRect().height}px;
+            top: ${hoveredEl.getBoundingClientRect().top}px;
+            left: ${hoveredEl.getBoundingClientRect().left}px;
+            opacity: 1;
+            transform: translate(0,0);
+            animation: none;
+            border-radius: ${getComputedStyle(hoveredEl).borderBottomLeftRadius};
+            transition: width .5s, height .5s, top .5s, left .5s, transform .5s, border-radius .5s;
+            `;
+        };
+
+        hoveredEl.onmouseleave = () => {
+            mouseCircleBool = true;
+        };
+
+        document.onscroll = () => {
+            if (!mouseCircleBool) {
+                mouseCircle.style.top = `${hoveredEl.getBoundingClientRect().top}px`;
+            }
+        };
+    }
+};
+// End of Mouse Circle Transform
+
 document.body.addEventListener("mousemove", (e) => {
     let x = e.clientX;
     let y = e.clientY;
 
     mouseCircleFn(x,y);
     animateCircles(e, x, y);
+
+    const hoveredEl = document.elementFromPoint(x,y);
+
+    stickyEl(x, y, hoveredEl);
+
+    mouseCircleTransform(hoveredEl);
+    
 });
 
 document.body.addEventListener("mouseleave", () => {
@@ -84,14 +152,28 @@ mainBtns.forEach(btn => {
 // End of Main Button
 
 // Progress Bar
+const sections = document.querySelectorAll("section");
+const progressBar = document.querySelector(".progress-bar");
 const halfCircles = document.querySelectorAll(".half-circle");
 const halfCircleTop = document.querySelector(".half-circle-top");
-const progressBarCircle = document.querySelector(".progress-bar-cirlce");
+const progressBarCircle = document.querySelector(".progress-bar-circle");
 
-const progressBarFn = () => {
+let scrolledPortion = 0;
+let scrollBool = false;
+let imageWrapper = false;
+
+const progressBarFn = (bigImgWrapper) => {
+    imageWrapper = bigImgWrapper;
+    let pageHeight = 0;
     const pageViewportHeight = window.innerHeight;
-    const pageHeight = document.documentElement.scrollHeight;
-    const scrolledPortion = window.pageYOffset;
+
+    if (!imageWrapper) {
+        pageHeight = document.documentElement.scrollHeight;
+        scrolledPortion = window.pageYOffset;
+    } else {
+        pageHeight = imageWrapper.firstElementChild.scrollHeight;
+        scrolledPortion = imageWrapper.scrollTop;
+    };
 
     const scrolledPortionDegree = (scrolledPortion / (pageHeight - pageViewportHeight)) * 360;
     
@@ -105,14 +187,48 @@ const progressBarFn = () => {
             halfCircleTop.style.opacity = "1";
         }
     });
+
+    scrollBool = scrolledPortion + pageViewportHeight === pageHeight;
+
+    // Arrow Rotation
+    if (scrollBool) {
+        progressBarCircle.style.transform = "rotate(180deg)";
+    } else {
+        progressBarCircle.style.transform = "rotate(0deg)";
+    }
+    // End of Arrow Rotation
 };
+
+    // Progress Bar Click
+    progressBar.addEventListener("click", e => {
+        e.preventDefault();
+
+        if (!imageWrapper) {
+            const sectionPositions = Array.from(sections).map(
+                section => scrolledPortion + section.getBoundingClientRect().top
+            );
+    
+            const position = sectionPositions.find((sectionPosition) => {
+                return sectionPosition > scrolledPortion;
+            });
+    
+            scrollBool ? window.scrollTo(0,0) : window.scrollTo(0, position);
+            console.log(position);
+        } else {
+            scrollBool ? imageWrapper.scrollTo(0,0) : imageWrapper.scrollTo(0, imageWrapper.scrollHeight);
+        }
+    });
+    // End of Progress Bar Click
+
+    progressBarFn();
+
 // End of Progress Bar
 
 // Navigation
 const menuIcon = document.querySelector(".menu-icon");
 const navbar = document.querySelector(".navbar");
 
-document.addEventListener("scroll", () => {
+const scrollFn = () => {
     menuIcon.classList.add("show-menu-icon");
     navbar.classList.add("hide-navbar");
     
@@ -122,7 +238,9 @@ document.addEventListener("scroll", () => {
     };
 
     progressBarFn();
-});
+};
+
+document.addEventListener("scroll", scrollFn);
 
 menuIcon.addEventListener("click", () => {
     menuIcon.classList.remove("show-menu-icon");
@@ -173,12 +291,24 @@ projects.forEach((project, i) => {
         bigImgWrapper.appendChild(bigImg);
         document.body.style.overflowY = "hidden";
 
+        document.removeEventListener("scroll", scrollFn);
+
+        mouseCircle.style.opacity = 0;
+
+        progressBarFn(bigImgWrapper);
+
+        bigImgWrapper.onscroll = () => {
+            progressBarFn(bigImgWrapper);
+        }
+
         projectHideBtn.classList.add("change");
 
         projectHideBtn.onclick = () => {
             projectHideBtn.classList.remove("change");
             bigImgWrapper.remove();
             document.body.style.overflowY = "scroll";
+
+            document.addEventListener("scroll", scrollFn);
         };
     });
     // End of Big Project Image
@@ -300,4 +430,58 @@ setInterval(() => {
     }, 500);    
 }, 3000);
 // End of Slideshow
+
+// Form validation
+const form = document.querySelector(".contact-form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const messages = document.querySelectorAll(".message");
+
+const error = (input, message) => {
+    input.nextElementSibling.classList.add("error");
+    input.nextElementSibling.textContent = message;
+};
+
+const success = (input) => {
+    input.nextElementSibling.classList.remove("error");
+}
+
+const checkRequiredFields = (inputArr) => {
+    inputArr.forEach((input) => {
+        if (input.value.trim() === ""){
+            error(input, `${input.id} is required`);
+        }
+    });
+};
+
+const checkLength = (input, min) => {
+    if (input.value.trim().length < min) {
+        error(input, `${input.id} must be at least ${min} characters`);
+    } else {
+        success(input);
+    }
+};
+
+const checkEmail = (input) => {
+    const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (regEx.test(input.value.trim())) {
+        success(input);
+    } else {
+        error(input, "Email is not valid");
+    }
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkLength(username, 2);
+    checkLength(subject, 2);
+    checkLength(message, 10);
+    checkEmail(email);
+    checkRequiredFields([username, email, subject, message]);
+});
+// End of Form validation
 // End of Section 5
